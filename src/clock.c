a82f146b9eea32d3d694654f0947f20e3de87b9f
p-jacquot <pierre.jacquot@insa-lyon.fr>
2021-05-25 15:39:53 +0200

    Support for HermitCore.

--- src/clock.c
+++ src/clock.c
@@ -3,9 +3,22 @@
 #include <sys/time.h>
 #include <time.h>
 
+#ifndef GETTIMEOFDAY
+
 double get_time()
 {
     struct timespec t;
     clock_gettime(CLOCK_MONOTONIC, &t);
     return (double)(t.tv_sec + t.tv_nsec / 1000000000.0);
 }
+
+#else
+
+double get_time()
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return t.tv_sec + t.tv_usec / 1000000.0;
+}
+
+#endif // ifndef GETTIMEOFDAY
