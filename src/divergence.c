f028ded3bd4148ebe6a7918d7102e75925cd44df
p-jacquot <pierre.jacquot@insa-lyon.fr>
2021-05-28 11:38:21 +0200

    Make each thread put timestamps into its own array

--- src/divergence.c
+++ src/divergence.c
@@ -1,6 +1,7 @@
 #include "divergence.h"
 
 #include <omp.h>
+#include <string.h>
 
 #include "args.h"
 #include "clock.h"
@@ -23,7 +24,8 @@ int main(int argc, char ** argv)
 void diverge(Experience_s * exp)
 {
     unsigned int thread_num = omp_get_thread_num();
-    double * timestamps = exp->time_lists[thread_num];
+    size_t length = sizeof(double) * exp->measures;
+    double * timestamps = (double *) malloc(length);
 
 #pragma omp barrier
     
@@ -32,6 +34,8 @@ void diverge(Experience_s * exp)
         timestamps[i] = get_time();
         naive_cos(0.00001, exp->ncos);
     }
+    memcpy(exp->time_lists[thread_num], timestamps, length);
+    free(timestamps);
 }
 
 double naive_cos(double x, const unsigned int n)
